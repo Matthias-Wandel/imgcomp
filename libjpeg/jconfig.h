3dfebd118e7ec1f7a5f4349a8e41ab3c70b963ed
Matthias on i7 <mwandel@sentex.ca>
2015-01-04 17:00:26 -0500

    make library compile on raspberry pi

--- libjpeg/jconfig.h
+++ libjpeg/jconfig.h
@@ -37,7 +37,9 @@ typedef unsigned char boolean;
 #define TARGA_SUPPORTED		/* Targa image file format */
 
 #define TWO_FILE_COMMANDLINE	/* optional */
-#define USE_SETMODE		/* Microsoft has setmode() */
+#ifdef _WIN32
+    #define USE_SETMODE		/* Microsoft has setmode() */
+#endif
 #undef NEED_SIGNAL_CATCHER
 #undef DONT_USE_B_MODE
 #undef PROGRESS_REPORT		/* optional */
