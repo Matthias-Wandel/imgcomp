fdc29774833308f14be93bb5ca8fa123dccf4113
Matthias on i7 <mwandel@sentex.ca>
2015-01-04 20:18:32 -0500

    More stuff removed and styling

--- djpeg.c
+++ djpeg.c
@@ -1,7 +1,4 @@
 /*
- * This file contains a command-line user interface for the JPEG decompressor.
- * It should work on any system with Unix- or MS-DOS-style command lines.
- *
  * Two different command line styles are permitted, depending on the
  * compile-time switch TWO_FILE_COMMANDLINE:
  *	djpeg [options]  inputfile outputfile
@@ -18,76 +15,20 @@
  */
 #include "jconfig.h"
 #include "libjpeg/cdjpeg.h"		/* Common decls for cjpeg/djpeg applications */
-#include "libjpeg/jversion.h"		/* for version message */
-
-//#include <ctype.h>		/* to declare isprint() */
-
-#ifdef USE_CCOMMAND		/* command-line reader for Macintosh */
-#ifdef __MWERKS__
-#include <SIOUX.h>              /* Metrowerks needs this */
-#include <console.h>		/* ... and this */
-#endif
-#ifdef THINK_C
-#include <console.h>		/* Think declares it here */
-#endif
-#endif
 
 
-#undef BMP_SUPPORTED		/* BMP image file format */
-#undef GIF_SUPPORTED		/* GIF image file format */
-//#undef PPM_SUPPORTED		/* PBMPLUS PPM/PGM image file format */
-#undef TARGA_SUPPORTED		/* Targa image file format */
-
-
-/* Create the add-on message string table. */
-
-#define JMESSAGE(code,string)	string ,
-
+// Create the add-on message string table.
 static const char * const cdjpeg_message_table[] = {
 #include "libjpeg/cderror.h"
   NULL
 };
 
 
-/*
- * This list defines the known output image formats
- * (not all of which need be supported by a given version).
- * You can change the default output format by defining DEFAULT_FMT;
- * indeed, you had better do so if you undefine PPM_SUPPORTED.
- */
-
-typedef enum {
-	FMT_BMP,		/* BMP format (Windows flavor) */
-	FMT_GIF,		/* GIF format */
-	FMT_OS2,		/* BMP format (OS/2 flavor) */
-	FMT_PPM,		/* PPM/PGM (PBMPLUS formats) */
-	FMT_RLE,		/* RLE format */
-	FMT_TARGA,		/* Targa format */
-	FMT_TIFF		/* TIFF format */
-} IMAGE_FORMATS;
-
-#ifndef DEFAULT_FMT		/* so can override from CFLAGS in Makefile */
-#define DEFAULT_FMT	FMT_PPM
-#endif
-
-static IMAGE_FORMATS requested_fmt;
+static const char * progname;  // program name for error messages
+static char * outfilename;	   // for -outfile switch
 
 
-/*
- * Argument-parsing code.
- * The switch parser is designed to be useful with DOS-style command line
- * syntax, ie, intermixed switches and file names, where only the switches
- * to the left of a given file name affect processing of that file.
- * The main program in this file doesn't actually use this capability...
- */
-
-
-static const char * progname;	/* program name for error messages */
-static char * outfilename;	/* for -outfile switch */
-
-
-void usage (void)
-/* complain about bad command line */
+void usage (void)// complain about bad command line 
 {
   fprintf(stderr, "usage: %s [switches] ", progname);
 #ifdef TWO_FILE_COMMANDLINE
@@ -101,14 +42,6 @@ void usage (void)
   fprintf(stderr, "  -grayscale     Force grayscale output\n");
 #ifdef IDCT_SCALING_SUPPORTED
   fprintf(stderr, "  -scale M/N     Scale output image by fraction M/N, eg, 1/8\n");
-#endif
-#ifdef BMP_SUPPORTED
-  fprintf(stderr, "  -bmp           Select BMP output format (Windows style)%s\n",
-	  (DEFAULT_FMT == FMT_BMP ? " (default)" : ""));
-#endif
-#ifdef PPM_SUPPORTED
-  fprintf(stderr, "  -pnm           Select PBMPLUS (PPM/PGM) output format%s\n",
-	  (DEFAULT_FMT == FMT_PPM ? " (default)" : ""));
 #endif
   fprintf(stderr, "Switches for advanced users:\n");
   fprintf(stderr, "  -nosmooth      Don't use high-quality upsampling\n");
@@ -129,223 +62,189 @@ static int parse_switches (j_decompress_ptr cinfo, int argc, char **argv,
  * processing.
  */
 {
-  int argn;
-  char * arg;
+    int argn;
+    char * arg;
 
-  /* Set up default JPEG parameters. */
-  requested_fmt = DEFAULT_FMT;	/* set default output file format */
-  outfilename = NULL;
-  cinfo->err->trace_level = 0;
+    // Set up default JPEG parameters.
+    outfilename = NULL;
+    cinfo->err->trace_level = 0;
 
-  /* Scan command line options, adjust parameters */
+    /* Scan command line options, adjust parameters */
 
-  for (argn = 1; argn < argc; argn++) {
-    arg = argv[argn];
-    if (*arg != '-') {
-      /* Not a switch, must be a file name argument */
-      if (argn <= last_file_arg_seen) {
-	outfilename = NULL;	/* -outfile applies to just one input file */
-	continue;		/* ignore this name if previously processed */
-      }
-      break;			/* else done parsing switches */
+    for (argn = 1; argn < argc; argn++) {
+        arg = argv[argn];
+        if (*arg != '-') {
+            /* Not a switch, must be a file name argument */
+            if (argn <= last_file_arg_seen) {
+                outfilename = NULL;	/* -outfile applies to just one input file */
+                continue;		/* ignore this name if previously processed */
+            }
+            break;			/* else done parsing switches */
+        }
+        arg++;			/* advance past switch marker character */
+
+        if (keymatch(arg, "debug", 1) || keymatch(arg, "verbose", 1)) {
+            // Enable debug printouts.  Specify more than once for more detail.
+            cinfo->err->trace_level++;
+
+        } else if (keymatch(arg, "fast", 1)) {
+            // Select recommended processing options for quick-and-dirty output.
+            cinfo->two_pass_quantize = FALSE;
+            cinfo->dct_method = JDCT_FASTEST;
+            cinfo->do_fancy_upsampling = FALSE;
+
+        } else if (keymatch(arg, "grayscale", 2) || keymatch(arg, "greyscale",2)) {
+            // Force monochrome output.
+            cinfo->out_color_space = JCS_GRAYSCALE;
+        } else if (keymatch(arg, "nosmooth", 3)) {
+            // Suppress fancy upsampling
+            cinfo->do_fancy_upsampling = FALSE;
+
+        } else if (keymatch(arg, "outfile", 4)) {
+            // Set output file name.
+            if (++argn >= argc)	/* advance to next argument */
+	           usage();
+            outfilename = argv[argn];	// save it away for later use
+
+        } else if (keymatch(arg, "scale", 1)) {
+            // Scale the output image by a fraction M/N. */
+            if (++argn >= argc)	/* advance to next argument */
+                 usage();
+            if (sscanf(argv[argn], "%d/%d", &cinfo->scale_num, &cinfo->scale_denom) != 2)
+               usage();
+
+        } else {
+            usage();	   // bogus switch
+        }
     }
-    arg++;			/* advance past switch marker character */
 
-    if (keymatch(arg, "bmp", 1)) {
-      /* BMP output format. */
-      requested_fmt = FMT_BMP;
-
-    } else if (keymatch(arg, "debug", 1) || keymatch(arg, "verbose", 1)) {
-      // Enable debug printouts.  Specify more than once for more detail.
-      cinfo->err->trace_level++;
-
-    } else if (keymatch(arg, "fast", 1)) {
-      // Select recommended processing options for quick-and-dirty output.
-      cinfo->two_pass_quantize = FALSE;
-      cinfo->dct_method = JDCT_FASTEST;
-      cinfo->do_fancy_upsampling = FALSE;
-
-    } else if (keymatch(arg, "grayscale", 2) || keymatch(arg, "greyscale",2)) {
-      /* Force monochrome output. */
-      cinfo->out_color_space = JCS_GRAYSCALE;
-    } else if (keymatch(arg, "nosmooth", 3)) {
-      /* Suppress fancy upsampling */
-      cinfo->do_fancy_upsampling = FALSE;
-
-    } else if (keymatch(arg, "onepass", 3)) {
-      /* Use fast one-pass quantization. */
-      cinfo->two_pass_quantize = FALSE;
-
-    } else if (keymatch(arg, "outfile", 4)) {
-      /* Set output file name. */
-      if (++argn >= argc)	/* advance to next argument */
-	usage();
-      outfilename = argv[argn];	/* save it away for later use */
-
-    } else if (keymatch(arg, "pnm", 1) || keymatch(arg, "ppm", 1)) {
-      /* PPM/PGM output format. */
-      requested_fmt = FMT_PPM;
-
-    } else if (keymatch(arg, "scale", 1)) {
-      /* Scale the output image by a fraction M/N. */
-      if (++argn >= argc)	/* advance to next argument */
-	usage();
-      if (sscanf(argv[argn], "%d/%d",
-		 &cinfo->scale_num, &cinfo->scale_denom) != 2)
-	usage();
-
-    } else {
-      usage();			/* bogus switch */
-    }
-  }
-
-  return argn;			/* return index of next arg (file name) */
+    return argn;		   // return index of next arg (file name)
 }
 
 
 //-----------------------------------------------------------------------------------
 // The main program.
 //-----------------------------------------------------------------------------------
-
 int main (int argc, char **argv)
 {
-  struct jpeg_decompress_struct cinfo;
-  struct jpeg_error_mgr jerr;
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
 
-  int file_index;
-  djpeg_dest_ptr dest_mgr = NULL;
-  FILE * input_file;
-  FILE * output_file;
-  JDIMENSION num_scanlines;
+    int file_index;
+    djpeg_dest_ptr dest_mgr = NULL;
+    FILE * input_file;
+    FILE * output_file;
+    JDIMENSION num_scanlines;
 
 printf("hello\n");
 
-  progname = argv[0];
+    progname = argv[0];
 
-  /* Initialize the JPEG decompression object with default error handling. */
-  cinfo.err = jpeg_std_error(&jerr);
-  jpeg_create_decompress(&cinfo);
-  /* Add some application-specific error messages (from cderror.h) */
-  jerr.addon_message_table = cdjpeg_message_table;
-  jerr.first_addon_message = JMSG_FIRSTADDONCODE;
-  jerr.last_addon_message = JMSG_LASTADDONCODE;
+    /* Initialize the JPEG decompression object with default error handling. */
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+    /* Add some application-specific error messages (from cderror.h) */
+    jerr.addon_message_table = cdjpeg_message_table;
+    jerr.first_addon_message = JMSG_FIRSTADDONCODE;
+    jerr.last_addon_message = JMSG_LASTADDONCODE;
 
 
-  /* Scan command line to find file names. */
-  /* It is convenient to use just one switch-parsing routine, but the switch
-   * values read here are ignored; we will rescan the switches after opening
-   * the input file.
-   * (Exception: tracing level set here controls verbosity for COM markers
-   * found during jpeg_read_header...)
-   */
+    /* Scan command line to find file names. */
+    /* It is convenient to use just one switch-parsing routine, but the switch
+     * values read here are ignored; we will rescan the switches after opening
+     * the input file.
+     * (Exception: tracing level set here controls verbosity for COM markers
+     * found during jpeg_read_header...)
+     */
 
   file_index = parse_switches(&cinfo, argc, argv, 0, FALSE);
 
 #ifdef TWO_FILE_COMMANDLINE
-  /* Must have either -outfile switch or explicit output file name */
-  if (outfilename == NULL) {
-    if (file_index != argc-2) {
-      fprintf(stderr, "%s: must name one input and one output file\n",
-	      progname);
-      usage();
+    /* Must have either -outfile switch or explicit output file name */
+    if (outfilename == NULL) {
+        if (file_index != argc-2) {
+            fprintf(stderr, "%s: must name one input and one output file\n", progname);
+            usage();
+        }
+      outfilename = argv[file_index+1];
+    } else {
+        if (file_index != argc-1) {
+            fprintf(stderr, "%s: must name one input and one output file\n",
+	            progname);
+            usage();
+        }
     }
-    outfilename = argv[file_index+1];
-  } else {
-    if (file_index != argc-1) {
-      fprintf(stderr, "%s: must name one input and one output file\n",
-	      progname);
-      usage();
-    }
-  }
 #else
-  /* Unix style: expect zero or one file name */
-  if (file_index < argc-1) {
-    fprintf(stderr, "%s: only one input file\n", progname);
-    usage();
-  }
-#endif /* TWO_FILE_COMMANDLINE */
-
-  /* Open the input file. */
-  if (file_index < argc) {
-    if ((input_file = fopen(argv[file_index], READ_BINARY)) == NULL) {
-      fprintf(stderr, "%s: can't open %s\n", progname, argv[file_index]);
-      exit(EXIT_FAILURE);
+    // Unix style: expect zero or one file name 
+    if (file_index < argc-1) {
+        fprintf(stderr, "%s: only one input file\n", progname);
+        usage();
     }
-  } else {
-    /* default input file is stdin */
-    input_file = read_stdin();
-  }
+#endif // TWO_FILE_COMMANDLINE
 
-  /* Open the output file. */
-  if (outfilename != NULL) {
-    if ((output_file = fopen(outfilename, WRITE_BINARY)) == NULL) {
-      fprintf(stderr, "%s: can't open %s\n", progname, outfilename);
-      exit(EXIT_FAILURE);
+    /* Open the input file. */
+    if (file_index < argc) {
+        if ((input_file = fopen(argv[file_index], READ_BINARY)) == NULL) {
+            fprintf(stderr, "%s: can't open %s\n", progname, argv[file_index]);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        /* default input file is stdin */
+        input_file = read_stdin();
     }
-  } else {
-    /* default output file is stdout */
-    output_file = write_stdout();
-  }
 
-  /* Specify data source for decompression */
-  jpeg_stdio_src(&cinfo, input_file);
+    // Open the output file.
+    if (outfilename != NULL) {
+        if ((output_file = fopen(outfilename, WRITE_BINARY)) == NULL) {
+            fprintf(stderr, "%s: can't open %s\n", progname, outfilename);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // default output file is stdout 
+        output_file = write_stdout();
+    }
 
-  /* Read file header, set default decompression parameters */
-  (void) jpeg_read_header(&cinfo, TRUE);
+    /* Specify data source for decompression */
+    jpeg_stdio_src(&cinfo, input_file);
 
-  /* Adjust default decompression parameters by re-parsing the options */
-  file_index = parse_switches(&cinfo, argc, argv, 0, TRUE);
+    /* Read file header, set default decompression parameters */
+    (void) jpeg_read_header(&cinfo, TRUE);
+
+    /* Adjust default decompression parameters by re-parsing the options */
+    file_index = parse_switches(&cinfo, argc, argv, 0, TRUE);
 
 printf("hello2\n");
 
-  /* Initialize the output module now to let it override any crucial
-   * option settings (for instance, GIF wants to force color quantization).
-   */
-  switch (requested_fmt) {
-    #ifdef BMP_SUPPORTED
-      case FMT_BMP:
-        dest_mgr = jinit_write_bmp(&cinfo, FALSE);
-        break;
-    #endif
-    #ifdef PPM_SUPPORTED
-      case FMT_PPM:
-        dest_mgr = jinit_write_ppm(&cinfo);
-        break;
-    #endif
-  default:
-    ERREXIT(&cinfo, JERR_UNSUPPORTED_FORMAT);
-    break;
-  }
-  dest_mgr->output_file = output_file;
+    dest_mgr = jinit_write_ppm(&cinfo);
+
+    dest_mgr->output_file = output_file;
 printf("hello3\n");
-  // Start decompressor
-  (void) jpeg_start_decompress(&cinfo);
+    // Start decompressor
+    (void) jpeg_start_decompress(&cinfo);
 
-  // Write output file header
-  (*dest_mgr->start_output) (&cinfo, dest_mgr);
+    // Write output file header
+    (*dest_mgr->start_output) (&cinfo, dest_mgr);
 
-  // Process data 
-  while (cinfo.output_scanline < cinfo.output_height) {
-    num_scanlines = jpeg_read_scanlines(&cinfo, dest_mgr->buffer,
-					dest_mgr->buffer_height);
-    (*dest_mgr->put_pixel_rows) (&cinfo, dest_mgr, num_scanlines);
-  }
+    // Process data 
+    while (cinfo.output_scanline < cinfo.output_height) {
+        num_scanlines = jpeg_read_scanlines(&cinfo, dest_mgr->buffer,
+    					  dest_mgr->buffer_height);
+        (*dest_mgr->put_pixel_rows) (&cinfo, dest_mgr, num_scanlines);
+    }
 
 printf("hello4\n");
-  // Finish decompression and release memory.
-  // I must do it in this order because output module has allocated memory
-  // of lifespan JPOOL_IMAGE; it needs to finish before releasing memory.
-  (*dest_mgr->finish_output) (&cinfo, dest_mgr);
-  (void) jpeg_finish_decompress(&cinfo);
-  jpeg_destroy_decompress(&cinfo);
+    // Finish decompression and release memory.
+    // I must do it in this order because output module has allocated memory
+    // of lifespan JPOOL_IMAGE; it needs to finish before releasing memory.
+    (*dest_mgr->finish_output) (&cinfo, dest_mgr);
+    (void) jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
 
-  // Close files, if we opened them
-  if (input_file != stdin)
-    fclose(input_file);
-  if (output_file != stdout)
-    fclose(output_file);
+    // Close files, if we opened them
+    if (input_file != stdin) fclose(input_file);
+    if (output_file != stdout) fclose(output_file);
 
-  // All done.
-  exit(jerr.num_warnings ? EXIT_WARNING : EXIT_SUCCESS);
-  return 0;			//* suppress no-return-value warnings */
+    // All done.
+    exit(jerr.num_warnings ? EXIT_WARNING : EXIT_SUCCESS);
+    return 0;			//* suppress no-return-value warnings */
 }
--- wrppm.c
+++ wrppm.c
@@ -1,67 +1,43 @@
-/*
- * This file contains routines to write output images in PPM/PGM format.
- * The extended 2-byte-per-sample raw PPM/PGM formats are supported.
- * The PBMPLUS library is NOT required to compile this software
- * (but it is highly useful as a set of PPM image manipulation programs).
- *
- * These routines may need modification for non-Unix environments or
- * specialized applications.  As they stand, they assume output to
- * an ordinary stdio stream.
- */
+//----------------------------------------------------------------------
+// This file contains routines to write output images in PPM/PGM format.
+//----------------------------------------------------------------------
 
-#include "libjpeg/cdjpeg.h"		/* Common decls for cjpeg/djpeg applications */
+#include "libjpeg/cdjpeg.h"	  // Common decls for cjpeg/djpeg applications
 
-#ifdef PPM_SUPPORTED
-
-/*
- * For 12-bit JPEG data, we either downscale the values to 8 bits
- * (to write standard byte-per-sample PPM/PGM files), or output
- * nonstandard word-per-sample PPM/PGM files.  Downscaling is done
- * if PPM_NORAWWORD is defined (this can be done in the Makefile
- * or in jconfig.h).
- * (When the core library supports data precision reduction, a cleaner
- * implementation will be to ask for that instead.)
- */
 #if BITS_IN_JSAMPLE == 8
 #else
 sfhdashfdkas
 #endif
 
-#define PUTPPMSAMPLE(ptr,v)  *ptr++ = (char) (v)
-#define BYTESPERSAMPLE 1
 #define PPM_MAXVAL 255
 
 
-/* Private version of data destination object */
-
+// Private version of data destination object 
 typedef struct {
-  struct djpeg_dest_struct pub;	/* public fields */
+    struct djpeg_dest_struct pub;	/* public fields */
 
-  /* Usually these two pointers point to the same place: */
-  char *iobuffer;		/* fwrite's I/O buffer */
-  JSAMPROW pixrow;		/* decompressor output buffer */
-  size_t buffer_width;		/* width of I/O buffer */
-  JDIMENSION samples_per_row;	/* JSAMPLEs per output row */
+    /* Usually these two pointers point to the same place: */
+    char *iobuffer;		/* fwrite's I/O buffer */
+    JSAMPROW pixrow;		/* decompressor output buffer */
+    size_t buffer_width;		/* width of I/O buffer */
+    JDIMENSION samples_per_row;	/* JSAMPLEs per output row */
 } ppm_dest_struct;
 
 typedef ppm_dest_struct * ppm_dest_ptr;
 
 
-/*
- * Write some pixel data.
- * In this module rows_supplied will always be 1.
- *
- * put_pixel_rows handles the "normal" 8-bit case where the decompressor
- * output buffer is physically the same as the fwrite buffer.
- */
-
-static void
-put_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
+//----------------------------------------------------------------------
+// Write some pixel data.
+// In this module rows_supplied will always be 1.
+//
+// put_pixel_rows handles the "normal" 8-bit case where the decompressor
+// output buffer is physically the same as the fwrite buffer.
+//----------------------------------------------------------------------
+static void put_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
 		JDIMENSION rows_supplied)
 {
-  ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
-
-  (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
+    ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
+    (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
 }
 
 
@@ -69,41 +45,38 @@ put_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
 //----------------------------------------------------------------------
 // Startup: write the file header.
 //----------------------------------------------------------------------
-
 static void start_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 {
-  ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
+    ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
 
-  /* Emit file header */
-  switch (cinfo->out_color_space) {
-  case JCS_GRAYSCALE:
-    /* emit header for raw PGM format */
-    fprintf(dest->pub.output_file, "P5\n%ld %ld\n%d\n",
-	    (long) cinfo->output_width, (long) cinfo->output_height,
-	    PPM_MAXVAL);
-    break;
-  case JCS_RGB:
-    /* emit header for raw PPM format */
-    fprintf(dest->pub.output_file, "P6\n%ld %ld\n%d\n",
-	    (long) cinfo->output_width, (long) cinfo->output_height,
-	    PPM_MAXVAL);
-    break;
-  default:
-    ERREXIT(cinfo, JERR_PPM_COLORSPACE);
-  }
+    // Emit file header
+    switch (cinfo->out_color_space) {
+    case JCS_GRAYSCALE:
+        // emit header for raw PGM format, grayscale
+        fprintf(dest->pub.output_file, "P5\n%ld %ld\n%d\n",
+	      (long) cinfo->output_width, (long) cinfo->output_height,
+	      PPM_MAXVAL);
+        break;
+    case JCS_RGB:
+        // emit header for raw PPM format, colour
+        fprintf(dest->pub.output_file, "P6\n%ld %ld\n%d\n",
+	      (long) cinfo->output_width, (long) cinfo->output_height,
+	      PPM_MAXVAL);
+        break;
+    default:
+        ERREXIT(cinfo, JERR_PPM_COLORSPACE);
+    }
 }
 
 
 //----------------------------------------------------------------------
 // Finish up at the end of the file.
 //----------------------------------------------------------------------
-
 static void finish_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 {
-  /* Make sure we wrote the output file OK */
-  fflush(dinfo->output_file);
-  if (ferror(dinfo->output_file))
-    ERREXIT(cinfo, JERR_FILE_WRITE);
+    // Make sure we wrote the output file OK
+    fflush(dinfo->output_file);
+    if (ferror(dinfo->output_file)) ERREXIT(cinfo, JERR_FILE_WRITE);
 }
 
 
@@ -112,34 +85,32 @@ static void finish_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 //----------------------------------------------------------------------
 djpeg_dest_ptr jinit_write_ppm (j_decompress_ptr cinfo)
 {
-  ppm_dest_ptr dest;
+    ppm_dest_ptr dest;
 
-  /* Create module interface object, fill in method pointers */
-  dest = (ppm_dest_ptr)
-      (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_IMAGE,
-				  SIZEOF(ppm_dest_struct));
-  dest->pub.start_output = start_output_ppm;
-  dest->pub.finish_output = finish_output_ppm;
+    // Create module interface object, fill in method pointers
+    dest = (ppm_dest_ptr)
+        (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_IMAGE,
+				    SIZEOF(ppm_dest_struct));
+    dest->pub.start_output = start_output_ppm;
+    dest->pub.finish_output = finish_output_ppm;
 
-  /* Calculate output image dimensions so we can allocate space */
-  jpeg_calc_output_dimensions(cinfo);
+    // Calculate output image dimensions so we can allocate space
+    jpeg_calc_output_dimensions(cinfo);
 
-  /* Create physical I/O buffer.  Note we make this near on a PC. */
-  dest->samples_per_row = cinfo->output_width * cinfo->out_color_components;
-  dest->buffer_width = dest->samples_per_row * (BYTESPERSAMPLE * SIZEOF(char));
-  dest->iobuffer = (char *) (*cinfo->mem->alloc_small)
-    ((j_common_ptr) cinfo, JPOOL_IMAGE, dest->buffer_width);
+    // Create physical I/O buffer.  
+    dest->samples_per_row = cinfo->output_width * cinfo->out_color_components;
+    dest->buffer_width = dest->samples_per_row;
+    dest->iobuffer = (char *) (*cinfo->mem->alloc_small)
+      ((j_common_ptr) cinfo, JPOOL_IMAGE, dest->buffer_width);
 
 
-  /* We will fwrite() directly from decompressor output buffer. */
-  /* Synthesize a JSAMPARRAY pointer structure */
-  /* Cast here implies near->far pointer conversion on PCs */
-  dest->pixrow = (JSAMPROW) dest->iobuffer;
-  dest->pub.buffer = & dest->pixrow;
-  dest->pub.buffer_height = 1;
-  dest->pub.put_pixel_rows = put_pixel_rows;
+    // We will fwrite() directly from decompressor output buffer.
+    // Synthesize a JSAMPARRAY pointer structure
+    // Cast here implies near->far pointer conversion on PCs
+    dest->pixrow = (JSAMPROW) dest->iobuffer;
+    dest->pub.buffer = & dest->pixrow;
+    dest->pub.buffer_height = 1;
+    dest->pub.put_pixel_rows = put_pixel_rows;
 
-  return (djpeg_dest_ptr) dest;
+    return (djpeg_dest_ptr) dest;
 }
-
-#endif /* PPM_SUPPORTED */
