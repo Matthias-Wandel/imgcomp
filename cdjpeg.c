ce5ffe49dfc38766f7ff67998a922a6cc5c982f4
Matthias on i7 <mwandel@sentex.ca>
2015-01-04 19:51:20 -0500

    Make it compile on raspberry pi

--- cdjpeg.c
+++ cdjpeg.c
@@ -5,12 +5,13 @@
 
 #include "libjpeg/cdjpeg.h"		/* Common decls for cjpeg/djpeg applications */
 #include <ctype.h>		/* to declare isupper(), tolower() */
+
 #ifdef USE_SETMODE
     #include <fcntl.h>		/* to declare setmode()'s parameter macros */
     /* If you have setmode() but not <io.h>, just delete this line: */
     #include <io.h>			/* to declare setmode() */
 #else
-    #define setmode(a, b);
+    #define _setmode(a, b);
 #endif
 
 
--- wrppm.c
+++ wrppm.c
@@ -1,10 +1,4 @@
 /*
- * wrppm.c
- *
- * Copyright (C) 1991-1996, Thomas G. Lane.
- * This file is part of the Independent JPEG Group's software.
- * For conditions of distribution and use, see the accompanying README file.
- *
  * This file contains routines to write output images in PPM/PGM format.
  * The extended 2-byte-per-sample raw PPM/PGM formats are supported.
  * The PBMPLUS library is NOT required to compile this software
@@ -36,17 +30,6 @@ sfhdashfdkas
 #define PUTPPMSAMPLE(ptr,v)  *ptr++ = (char) (v)
 #define BYTESPERSAMPLE 1
 #define PPM_MAXVAL 255
-
-
-/*
- * When JSAMPLE is the same size as char, we can just fwrite() the
- * decompressed data to the PPM or PGM file.  On PCs, in order to make this
- * work the output buffer must be allocated in near data space, because we are
- * assuming small-data memory model wherein fwrite() can't reach far memory.
- * If you need to process very wide images on a PC, you might have to compile
- * in large-memory model, or else replace fwrite() with a putc() loop ---
- * which will be much slower.
- */
 
 
 /* Private version of data destination object */
@@ -82,32 +65,10 @@ put_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
 }
 
 
-/*
- * This code is used when we have to copy the data and apply a pixel
- * format translation.  Typically this only happens in 12-bit mode.
- */
 
-static void
-copy_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
-		 JDIMENSION rows_supplied)
-{
-  ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
-  register char * bufferptr;
-  register JSAMPROW ptr;
-  register JDIMENSION col;
-
-  ptr = dest->pub.buffer[0];
-  bufferptr = dest->iobuffer;
-  for (col = dest->samples_per_row; col > 0; col--) {
-    PUTPPMSAMPLE(bufferptr, GETJSAMPLE(*ptr++));
-  }
-  (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
-}
-
-
-/*
- * Startup: write the file header.
- */
+//----------------------------------------------------------------------
+// Startup: write the file header.
+//----------------------------------------------------------------------
 
 static void start_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 {
@@ -133,9 +94,9 @@ static void start_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 }
 
 
-/*
- * Finish up at the end of the file.
- */
+//----------------------------------------------------------------------
+// Finish up at the end of the file.
+//----------------------------------------------------------------------
 
 static void finish_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 {
@@ -146,10 +107,9 @@ static void finish_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 }
 
 
-/*
- * The module selection routine for PPM format output.
- */
-
+//----------------------------------------------------------------------
+// The module selection routine for PPM format output.
+//----------------------------------------------------------------------
 djpeg_dest_ptr jinit_write_ppm (j_decompress_ptr cinfo)
 {
   ppm_dest_ptr dest;
@@ -169,7 +129,6 @@ djpeg_dest_ptr jinit_write_ppm (j_decompress_ptr cinfo)
   dest->buffer_width = dest->samples_per_row * (BYTESPERSAMPLE * SIZEOF(char));
   dest->iobuffer = (char *) (*cinfo->mem->alloc_small)
     ((j_common_ptr) cinfo, JPOOL_IMAGE, dest->buffer_width);
-
 
 
   /* We will fwrite() directly from decompressor output buffer. */
