f4b376f435d3e031c739a9ee49ebc4ad8e2087bf
Matthias <mwandel@sentex.ca>
2015-12-29 15:26:04 -0400

    Fix indentation and stuff

--- blink_camera_led.c
+++ blink_camera_led.c
@@ -1,15 +1,9 @@
 //
-//  How to access GPIO registers from C-code on the Raspberry-Pi
+//  This program based on "How to access GPIO registers from C-code on the Raspberry-Pi
 //  Example program
-//  15-January-2012
-//  Dom and Gert
-//  Revised: 15-Feb-2013
- 
-// Access from ARM Running Linux
  
 #define BCM2708_PERI_BASE        0x20000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
- 
  
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,8 +19,7 @@ void *gpio_map;
  
 // I/O access
 volatile unsigned *gpio;
- 
- 
+  
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
@@ -47,38 +40,38 @@ void setup_io();
  
 void printButton(int g)
 {
-  if (GET_GPIO(g)) // !=0 <-> bit is 1 <- port is HIGH=3.3V
-    printf("Button pressed!\n");
-  else // port is LOW=0V
-    printf("Button released!\n");
+	if (GET_GPIO(g)) // !=0 <-> bit is 1 <- port is HIGH=3.3V
+		printf("Button pressed!\n");
+	else // port is LOW=0V
+		printf("Button released!\n");
 }
  
 int main(int argc, char **argv)
 {
-  int g,rep;
+	int g,rep;
+
+	// Set up gpi pointer for direct register access
+	setup_io();
+
+	// Switch GPIO 7..11 to output mode
  
-  // Set up gpi pointer for direct register access
-  setup_io();
+	/************************************************************************\
+	* You are about to change the GPIO settings of your computer.          *
+	* Mess this up and it will stop working!                               *
+	* It might be a good idea to 'sync' before running this program        *
+	* so at least you still have your code changes written to the SD-card! *
+	\************************************************************************/
  
-  // Switch GPIO 7..11 to output mode
- 
- /************************************************************************\
-  * You are about to change the GPIO settings of your computer.          *
-  * Mess this up and it will stop working!                               *
-  * It might be a good idea to 'sync' before running this program        *
-  * so at least you still have your code changes written to the SD-card! *
- \************************************************************************/
- 
-  // Set GPIO pins 7-11 to output
-  INP_GPIO(32); // must use INP_GPIO before we can use OUT_GPIO
-  OUT_GPIO(32);
- 
-  // Turn on the camera LED once for 300 miliseconds
-  GPIO_SET2 = 1;
-  usleep(300000);
-  GPIO_CLR2 = 1;
- 
-  return 0;
+	// Set GPIO pins 7-11 to output
+	INP_GPIO(32); // must use INP_GPIO before we can use OUT_GPIO
+	OUT_GPIO(32);
+
+	// Turn on the camera LED once for 300 miliseconds
+	GPIO_SET2 = 1;
+	usleep(300000);
+	GPIO_CLR2 = 1;
+
+	return 0;
  
 } // main
  
@@ -88,31 +81,31 @@ int main(int argc, char **argv)
 //
 void setup_io()
 {
-   /* open /dev/mem */
-   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("can't open /dev/mem \n");
-      exit(-1);
-   }
- 
-   /* mmap GPIO */
-   gpio_map = mmap(
-      NULL,             //Any adddress in our space will do
-      BLOCK_SIZE,       //Map length
-      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-      MAP_SHARED,       //Shared with other processes
-      mem_fd,           //File to map
-      GPIO_BASE         //Offset to GPIO peripheral
-   );
- 
-   close(mem_fd); //No need to keep mem_fd open after mmap
- 
-   if (gpio_map == MAP_FAILED) {
-      printf("mmap error %d\n", (int)gpio_map);//errno also set!
-      exit(-1);
-   }
- 
-   // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
- 
+    // open /dev/mem 
+	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+	    printf("can't open /dev/mem \n");
+	    exit(-1);
+	}
+
+	/* mmap GPIO */
+	gpio_map = mmap(
+		NULL,             //Any adddress in our space will do
+		BLOCK_SIZE,       //Map length
+		PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+		MAP_SHARED,       //Shared with other processes
+		mem_fd,           //File to map
+		GPIO_BASE         //Offset to GPIO peripheral
+	);
+
+	close(mem_fd); //No need to keep mem_fd open after mmap
+
+	if (gpio_map == MAP_FAILED) {
+		printf("mmap error %d\n", (int)gpio_map);//errno also set!
+		exit(-1);
+	}
+
+	// Always use volatile pointer!
+	gpio = (volatile unsigned *)gpio_map;
+
  
 } // setup_io
