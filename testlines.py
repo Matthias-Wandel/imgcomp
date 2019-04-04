7702fdc82afd4766c0d876295b673523cd569d3a
Matthias <matthias@woodgears.ca>
2019-04-03 22:29:04 -0300

    Script for testing three stepper drivers hooked up to raspberry pi

---
+++ testlines.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# Python script for testing three stepper drivers hooked up to raspberry pi
+
+# Motor 1:
+# Enable: GPIO 02
+# Dir:    GPIO 03
+# Clock:  GPIO 04
+
+# Motor 2:
+# Enable: GPIO 15
+# Dir:    GPIO 17
+# Clock:  GPIO 18
+
+# Motor 3:
+# Enable: GPIO 22
+# Dir:    GPIO 23
+# Clock:  GPIO 24
+
+
+import RPi.GPIO as GPIO, sys
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# motor 1
+GPIO.setup(2, GPIO.OUT, initial=True)
+GPIO.setup(3, GPIO.OUT, initial=False)
+GPIO.setup(4, GPIO.OUT, initial=False)
+
+# motor 2
+GPIO.setup(15, GPIO.OUT, initial=True)
+GPIO.setup(17, GPIO.OUT, initial=False)
+GPIO.setup(18, GPIO.OUT, initial=False)
+
+# motor 3
+GPIO.setup(22, GPIO.OUT, initial=True)
+GPIO.setup(23, GPIO.OUT, initial=False)
+GPIO.setup(24, GPIO.OUT, initial=False)
+
+
+#GPIO.output(2,0);
+#GPIO.output(3,0);
+#GPIO.output(4,0);
+
+#GPIO.output(15,1);
+#GPIO.output(17,1);
+#GPIO.output(18,1);
+
+#GPIO.output(22,0);
+#GPIO.output(23,0);
+#GPIO.output(24,0);
+
+#sys.exit(0);
+
+duration = 0.5
+steps = 48
+motor = 1
+dir = 0
+delay = 0.0005
+
+if len(sys.argv) > 1:
+    motor = int(float(sys.argv[1]))
+    print("Motor: ",motor)
+    
+if len(sys.argv) > 2 and sys.argv[2] == "l":
+    print("Dir: left")
+    dir = 0
+elif len(sys.argv) > 2 and sys.argv[2] == "r":
+    print("Dir: right")
+    dir = 1
+    
+if len(sys.argv) > 3:
+    steps = int(float(sys.argv[3]))
+
+    
+if motor == 1:
+    g_enable = 2
+    g_dir = 3
+    g_clk = 4
+if motor == 2:
+    g_enable = 15
+    g_dir = 17
+    g_clk = 18
+if motor == 3:
+    g_enable = 22
+    g_dir = 23
+    g_clk = 24
+
+print("Motor:"+str(motor)+ "  Dir:"+str(dir)+"  Steps:"+str(steps))
+GPIO.output(g_enable, 0) # enable
+GPIO.output(g_dir, dir)
+for x in range (0, steps):
+    GPIO.output(g_clk,1)
+    sleep(delay)
+    GPIO.output(g_clk,0)
+    sleep(delay)
+GPIO.output(g_enable, 1)
+GPIO.output(g_dir, 0)
+
+
