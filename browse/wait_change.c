e106d376e28bfcd06ed5472a41908ac98322a2e9
Matthias wandel <matthias@woodgears.ca>
2019-10-22 10:34:56 -0300

    realtime display now sort of work, try on a different pi

---
+++ browse/wait_change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+//----------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------
+int main()
+{
+    int n, GotBytes = 0;
+    FILE * LogFile;
+    
+    printf("Content-Type: text/html\n\n<html>\n"); // html header
+    
+    LogFile = fopen("/ramdisk/log.txt", "r");
+    if (LogFile == NULL){
+        printf("Error!  No log.txt file\n");
+        sleep(1); // To keep from cycling too fast.
+        return 0;
+    }
+    fseek(LogFile, 0, SEEK_END);
+    
+    for (n=0;n<30;n++){
+        char NewBytes[100];
+        int nread;
+        usleep(200000);
+        nread = fread(NewBytes, 1, 100, LogFile);
+        if (nread > 0){
+            GotBytes = 1;
+            NewBytes[nread] = 0;
+            printf("%s<br>",NewBytes);
+            if (strstr(NewBytes, "(")){
+                // Saw enough motion to show location (though not necessarily enough to save image).
+                // Enouch change for live mode update.
+                break;
+            }
+        }
+    }
+    
+    if (GotBytes == 0){
+        printf("Error! log.txt not updating\n");
+    }
+    
+    // If more than 2 seconds without change, print error messages.
+    // Extract motion level, if it's above some threshold, call it motion (use cofig file?)
+    return 0;
+}
