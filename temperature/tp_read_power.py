bc67868a4bfca01375a6dc897fb96cd946be84ae
Matthias Wandel <matthias@woodgears.ca>
2019-12-05 09:45:37 -0400

    script for reading tplink hs110 power

--- temperature/tp_read_power.py
+++ temperature/tp_read_power.py
@@ -29,7 +29,7 @@ def decrypt(string):
     return result
 
 def read_power (ip_octet):
-#    try:
+    try:
         sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock_tcp.connect(("192.168.0."+str(ip_octet), 9999))
         sock_tcp.send(encrypt('{"emeter":{"get_realtime":{}}}'))
@@ -42,25 +42,23 @@ def read_power (ip_octet):
         resdict = json.loads(resstr)
         readings = resdict["emeter"]["get_realtime"]
         return readings["voltage"], readings["current"], readings["power"]
-#    except:
-#        return 0,0,0
+    except:
+        return 0,0,0
     
-#v,c,p = read_power(133)    
-##print (v,c,p)
-#print (f"133:  {v:6.2f}V {c:6.3f}A {p:6.2f}W ", end="    ")
 
 def readtostr():
     v,c,p = read_power(113)    
     timestr = strftime("%d-%b-%y %H:%M:%S", localtime())
-    return f"{timestr},{v:6.2f},{c:6.3f},{p:6.2f}"
-#print (v,c,p)
+    if v == 0: return "%s,,," % (timestr)
+    return "%s,%5.1f,%5.2f,%4.0f" % (timestr,v,c,p)
+    
 log1 = readtostr()
 print(log1)
-sleep(2)
+sleep(30)
 log2 = readtostr()
 print (log2)
 
-with open("furnace.txt", "a") as myfile:
+with open("furnace/furnace.txt", "a") as myfile:
     myfile.write(log1+"\n"+log2+"\n")
 
 
