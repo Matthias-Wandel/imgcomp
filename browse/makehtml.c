8c04551f29ec9e9a4c04f1cecf31436f4a1168c3
Matthias Wandel <matthias@woodgears.ca>
2020-01-22 11:29:18 -0400

    Highlight weekend and holidas

--- browse/makehtml.c
+++ browse/makehtml.c
@@ -19,6 +19,51 @@
 #include "view.h"
 
 //----------------------------------------------------------------------------------
+// Determine if it's a weekend day or a holiday
+//----------------------------------------------------------------------------------
+static int IsWeekendString(char * DirString)
+{
+    int a, daynum, weekday;
+    int y,m,d;
+    daynum=0;
+    for (a=0;a<6;a++){
+        if (DirString[a] < '0' || DirString[a] > '9') break;
+        daynum = daynum*10+DirString[a]-'0';
+    }
+    if (a != 6 || DirString[6] != '\0'){
+        // dir name must be six digits only.
+
+        return -2;
+    }
+
+    d = daynum%100;
+    m = (daynum/100)%100;
+    y = daynum/10000 + 2000;
+    //printf("%d %d %d",d,m,y);
+
+    {
+        // A clever bit of code from the internet!
+        static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+        y -= m < 3;
+        weekday = ( y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
+        // Sunday = 0, Saturday = 6
+    }
+
+    // New brunswick holidays thru end of 2021.
+    static const int Holidays[] = {190701, 190902, 191225,
+                200101, 200217, 200410, 200518, 200701, 200907, 201225,
+                210101, 210402, 210524, 210701, 210802, 210906, 211227};
+
+    for (a=0;a<sizeof(Holidays)/sizeof(int);a++){
+        // Check if date is a holday.
+        if (Holidays[a] == daynum) return weekday;
+    }
+
+    if (weekday == 0 || weekday == 6) return weekday;
+    return -1;
+}
+
+//----------------------------------------------------------------------------------
 // Do actagram output
 //----------------------------------------------------------------------------------
 void ShowActagram(int all, int h24)
@@ -30,6 +75,7 @@ void ShowActagram(int all, int h24)
     printf(
         "<style type=text/css>\n"
         "  body { font-family: sans-serif; font-size: 20;}\n"
+        "  span.wkend {background-color: #E8E8E8}\n"
         "  pre { font-size: 10;}\n"
         "  a {text-decoration: none;}\n"
         "</style>\n");
@@ -43,7 +89,7 @@ void ShowActagram(int all, int h24)
     if (all){
         daynum = 0;
     }else{
-        daynum = DayDirs.NumEntries-30;
+        daynum = DayDirs.NumEntries-45;
     }
     
     if (daynum < 0) daynum = 0;
@@ -56,6 +102,9 @@ void ShowActagram(int all, int h24)
         if (strcmp(DayDirs.Entries[daynum].Name, "keep") == 0) continue;
         
         memset(bins, 0, sizeof(bins));
+
+        int isw = IsWeekendString(DayDirs.Entries[daynum].Name);
+        if (isw >= 0) printf("<span class=\"wkend\">");
         
         printf("<a href='view.cgi?%s'>%s</a>",DayDirs.Entries[daynum].Name, DayDirs.Entries[daynum].Name);
         sprintf(DirName, "pix/%s",DayDirs.Entries[daynum].Name);
@@ -110,11 +159,11 @@ void ShowActagram(int all, int h24)
                 putchar(nc);
             }
         }
+        if (isw >= 0) printf("</span>");
         printf("\n");
     }
     free(DayDirs.Entries);
 }
-
 
 //----------------------------------------------------------------------------------
 // Create browsable HTML index for a directory
@@ -156,7 +205,8 @@ void MakeHtmlOutput(Dir_t * Dir)
         "  body { font-family: sans-serif; font-size: 24;}\n"
         "  img { vertical-align: middle; margin-bottom: 5px; }\n"
         "  p {margin-bottom: 0px}\n"
-        "  span {font-family: courier; font-weight: bold; font-size: 14px;}\n"
+        "  span.a {font-family: courier; font-weight: bold; font-size: 14px;}\n"
+        "  span.wkend {background-color: #E8E8E8}\n"
         "  a {text-decoration: none;}\n"
         "  div.ag { float:left; border-left: 1px solid black; margin-bottom:10px;}\n"
         "  div.pix { float:left; width:321px; height:%dpx;}\n", ThumbnailHeight+45);
@@ -206,8 +256,13 @@ void MakeHtmlOutput(Dir_t * Dir)
         
         if (strcmp(Directories.Entries[b].Name, "keep") == 0) continue;
         
+        int isw = -1;
+        if (IsRoot) isw = IsWeekendString(Directories.Entries[b].Name);
+        
         printf("<div class='ag'>\n");
+        if (isw >= 0) printf("<span class=\"wkend\">");
         printf("<a href=\"view.cgi?%s/%s\">%s:</a>",Dir->HtmlPath, Directories.Entries[b].Name, Directories.Entries[b].Name);
+        if (isw >= 0) printf("</span>");
         
         // Count how many images in subdirectory.
         snprintf(SubdirName,210,"pix/%s/%s",Dir->HtmlPath, Directories.Entries[b].Name);
@@ -230,7 +285,7 @@ void MakeHtmlOutput(Dir_t * Dir)
                 if (binno >= 0 && binno < 20) Bins[binno] += 1;
             }
         
-            printf("<span>");
+            printf("<span.a>");
             for (a=0;a<20;a++){
                 if (Bins[a] > 4){
                     char nc = '-';
@@ -243,12 +298,13 @@ void MakeHtmlOutput(Dir_t * Dir)
                     printf("&nbsp;");
                 }
             }
-            printf("</span>\n");
+            printf("</span.a>\n");
         }
         
         free(SubdImages.Entries);
         
         printf("</div>\n");
+        if (IsRoot && isw == 6) printf("<br clear=left>");
         
     }
     
