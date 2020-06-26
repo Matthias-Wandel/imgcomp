46892cc73510d7a279b233393b69fc7d92535665
Matthias Wandel <matthias@woodgears.ca>
2020-06-26 09:57:54 -0300

    Handle case where no images written or nothing appended to log.  Also tabs to spaces

--- browse/wait_change.c
+++ browse/wait_change.c
@@ -22,55 +22,54 @@ int RefreshEveryFrame = 0;
 //----------------------------------------------------------------------------------
 int WaitForNewFile(char * Dir)
 {
-	int fd = inotify_init();
-	if (fd < 0) perror("inotify_init");
+    int fd = inotify_init();
+    if (fd < 0) perror("inotify_init");
 
-	// raspistill creates the file first with a name such as img1234.jpg~
-	// After writing the file, it's renamed to img1234.jpg.  It would make sense to
-	// also wait for IN_CLOSE_WRITE in case files are just copied there, however,
-	// that will trigger on IN_CLOSE_WRITE even, and by the next time we poll,
-	// the image will already be move,d so we miss it.
+    // raspistill creates the file first with a name such as img1234.jpg~
+    // After writing the file, it's renamed to img1234.jpg.  It would make sense to
+    // also wait for IN_CLOSE_WRITE in case files are just copied there, however,
+    // that will trigger on IN_CLOSE_WRITE even, and by the next time we poll,
+    // the image will already be move,d so we miss it.
 
-	inotify_add_watch( fd, "/ramdisk", IN_MOVED_TO);
+    inotify_add_watch( fd, "/ramdisk", IN_MOVED_TO);
 
-	// Wait for more files to appear.
-	struct pollfd pfd = { fd, POLLIN, 0 };
-	union {
-		struct inotify_event iev;
-		char padding [ sizeof(struct inotify_event)+100];
-	}ievc;
+    // Wait for more files to appear.
+    struct pollfd pfd = { fd, POLLIN, 0 };
+    union {
+        struct inotify_event iev;
+        char padding [ sizeof(struct inotify_event)+100];
+    }ievc;
 
-	for (;;){
-		int ret = poll(&pfd, 1, 5000);
-		//printf("poll ret = %d\n",ret);
-		if (ret < 0) {
-			printf("Error: select failed: %s\n", strerror(errno));
-			return 0;
-		}
+    for (;;){
+        int ret = poll(&pfd, 1, 5000);
+        if (ret < 0) {
+            printf("Error: select failed: %s\n", strerror(errno));
+            return 0;
+        }
 
-		if (ret == 0){
-			// Timeout waiting for a new file.
-			printf("Error: No new images\n");
-			return 0;
-		}
-		//printf("revents=%d\n",pfd.revents);
+        if (ret == 0){
+            // Timeout waiting for a new file.
+            printf("Error: No new images\n");
+            return 0;
+        }
+        //printf("revents=%d\n",pfd.revents);
 
-		ievc.iev.len = 50;
-		read(fd, &ievc, sizeof(ievc));
+        ievc.iev.len = 50;
+        read(fd, &ievc, sizeof(ievc));
 
-		int nlen = strlen(ievc.iev.name);
-		if (nlen > 4 && strcmp(ievc.iev.name+nlen-4, ".jpg") == 0){
-			break;
-		}else{
-			printf("%s\n", ievc.iev.name);
-		}
-	}
+        int nlen = strlen(ievc.iev.name);
+        if (nlen > 4 && strcmp(ievc.iev.name+nlen-4, ".jpg") == 0){
+            break;
+        }else{
+            printf("%s\n", ievc.iev.name);
+        }
+    }
 
-	if (RefreshEveryFrame){
-		printf("%s\n", ievc.iev.name);
-	}else{
-		printf("Please ensure /ramdisk/log.txt is enabled in imgcomp.conf\n");
-	}
+    if (RefreshEveryFrame){
+        printf("%s\n", ievc.iev.name);
+    }else{
+        printf("Please ensure /ramdisk/log.txt is enabled in imgcomp.conf\n");
+    }
     return 0;
 }
 
@@ -88,22 +87,22 @@ int main()
 
     printf("Content-Type: text/html\n\n<html>\n"); // html header
 
-	char * QueryString = getenv("QUERY_STRING");
-	if (QueryString && QueryString[0] == '1'){
-		RefreshEveryFrame = 1;
-	}
+    char * QueryString = getenv("QUERY_STRING");
+    if (QueryString && QueryString[0] == '1'){
+        RefreshEveryFrame = 1;
+    }
 
     // First try to follow symlink "in" in the current directory.  Only if that fails,
     // use the hardcoded path to ramdisk.
-	// I should really put this configuration in browse.cfg
+    // I should really put this configuration in browse.cfg
     WATCH_FILE = "in/log.txt";
-	LogFile = fopen(WATCH_FILE,"r");
+    LogFile = fopen(WATCH_FILE,"r");
 
-	if (RefreshEveryFrame && LogFile == NULL){
-		// Just look for new files coming into existence, we aren't
-		// looking for motion.
-		return WaitForNewFile("/ramdisk");
-	}
+    if (RefreshEveryFrame && LogFile == NULL){
+        // Just look for new files coming into existence, we aren't
+        // looking for motion.
+        return WaitForNewFile("/ramdisk");
+    }
 
     if (LogFile == NULL) {
         WATCH_FILE = "/ramdisk/log.txt";
@@ -111,9 +110,9 @@ int main()
     }
 
     if (LogFile == NULL){
-		sleep(1);
-		printf("No /ramdisk/log.txt found.  Please enable in imgcomp.conf\n");
-		return 0;
+        sleep(1);
+        printf("No /ramdisk/log.txt found.  Please enable in imgcomp.conf\n");
+        return 0;
     }
 
     fseek(LogFile, 0, SEEK_END);
@@ -129,25 +128,32 @@ int main()
     for (n=0;n<20;n++){
         char NewBytes[100];
         int nread;
-        long NewSize;
-printf("n=%d\n",n);
+
         if (time(NULL) >= now+5) break; // Wait at most five seconds.
-        // read to wait for change.
+
+        // poll to make sure a change is ready so the following read doesn't
+		// get stuck if the log file isn't being updated.
+        struct pollfd pfd = { infd, POLLIN, 0 };
+        int ret = poll(&pfd, 1, 1000);
+        if (ret == 0){
+			// no change for one second.
+            continue;
+        }
+
         read( infd, buffer, EVENT_BUF_LEN );
-printf("zoot\n");
-        // the following hack is necessary to work around a
-        // file system bug on the raspian
+
+        // the following hack is necessary because just reading from the end of the file again
+		// doesn't return anything even if the file grew since the last read.
         fseek(LogFile, 0, SEEK_END);
-        NewSize = ftell(LogFile);
+        long NewSize = ftell(LogFile);
         if (NewSize != FileSize){
             // Go back to read what was appended
             fseek(LogFile, FileSize, SEEK_SET);
             FileSize = NewSize;
         }else{
-            // File wasn't appended to.
-            continue;
+            continue; // File wasn't appended to.
         }
-printf("call fread %d\n",NewSize-FileSize);
+
         nread = fread(NewBytes, 1, 99, LogFile);
         if (nread > 0){
             GotBytes = 1;
@@ -161,11 +167,14 @@ printf("call fread %d\n",NewSize-FileSize);
         }
     }
 
+    // After 5 seconds, if nothing written to log file, its an error.
+	// If stuff was written to log file, stop regardless (so javascript returns and image refreshes anyway)
+
     if (GotBytes == 0){
-        printf("Error! log.txt not updating\n");
+		// If more than 5 seconds without change, print error messages.
+		// Extract motion level, if it's above some threshold, call it motion (use cofig file?)
+        printf("Error! log.txt not updating.  Imgcomp not running?\n");
     }
 
-    // If more than 2 seconds without change, print error messages.
-    // Extract motion level, if it's above some threshold, call it motion (use cofig file?)
     return 0;
 }
