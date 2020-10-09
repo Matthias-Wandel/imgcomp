9d7273fe06f187b709734b9e81c41c7d2302861b
Matthias Wandel <matthias@woodgears.ca>
2020-10-09 08:31:40 -0300

    Fix free block claculation to correspond with what 'df' commands puts out

--- scripts/free_up_space.py
+++ scripts/free_up_space.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 # Script to check free space on file system written to by imgcomp.
-# If less then 20% free space, erase the oldes date image directory, but only one directory
+# If less then 20% free space, erase the oldest date image directory, but only one directory
 # so script should be run more than once a day in case daily usage is higher than it was earlier.
+#
+# Crontab entry to run it every 6 hours, 7 minutes into the hour.
+# 7 0,6,12,18 * * * /home/pi/imgcomp/scripts/free_up_space.py >> /home/pi/freelog.txt
+
 import os, glob,sys
 from time import localtime, strftime, time
 
 timestr = strftime("%d-%b-%y %H:%M", localtime())
 
-xx=os.statvfs('/')
-total_blocks=xx.f_blocks
-free_blocks=xx.f_bfree
-percent_free = free_blocks/total_blocks*100
+xx=os.statvfs('/home/pi/images')
+total_blocks=xx.f_blocks*xx.f_bsize/1024
+avail_blocks=xx.f_bavail*xx.f_bsize/1024
+percent_avail = avail_blocks/total_blocks*100
 
-print(timestr+" %%%d (%3.1fG) free."%(percent_free, free_blocks/1024/1024), end='')
+print(timestr+" %%%d (%3.1fG) free."%(percent_avail, avail_blocks/1024/1024), end='')
 
 # If disk usage < 80%, do nothing.
-if percent_free > 20:
+if percent_avail > 20:
     print()
     sys.exit();
 
@@ -33,7 +37,7 @@ for name in dirs:
     break
 
 if wipedir:
-    print("  Del:'",datedir, end="'")    
+    print("  Del:'"+datedir, end="'")    
 
     now = time()
     import shutil
@@ -41,9 +45,9 @@ if wipedir:
     shutil.rmtree(name)
     
     xx=os.statvfs('/')
-    free_after=xx.f_bfree
+    avail_after=xx.f_bavail*xx.f_bsize/1024
     now2 = time()
-    print(" %4.2fG freedm t=%d"%((free_after-free_blocks)/1024/1024, (now2-now)))
+    print(" %4.2fG freedm t=%d"%((avail_after-avail_blocks)/1024/1024, (now2-now)))
     
 else:
     print ("No dir to delete");
