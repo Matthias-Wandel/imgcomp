59add119258931aa2107dcc55c0a1caf57858fbe
Matthias <matthias@woodgears.ca>
2020-11-03 14:23:20 -0400

    Lots more work on new timelapse script

--- scripts/timelapse.py
+++ scripts/timelapse.py
@@ -5,148 +5,208 @@
 # Matthias Wandel Nov 2020
 from PIL import Image, ImageDraw, ImageFont
 from subprocess import Popen, PIPE
-import sys, glob, os
-framerate = 10
-duration = 10
+import sys, glob, os, re, argparse, shutil
+framerate = 7.5
+
+if shutil.which("ffmpeg") == None:
+    print("[Error]: ffmpeg not found in PATH", file=sys.stderr)
+    exit(1)
+
+parser = argparse.ArgumentParser(
+    description='Make timelapses with images from imgcomp using ffmpeg.',
+    allow_abbrev=True,
+)
+parser.add_argument(
+    '-o','--outputfile',
+    required=False,
+    help='Output file name.',
+)
+
+parser.add_argument(
+    '-n', '--no-timestamp',
+    required=False, default=False,
+    action='store_true',
+    help='Flag to disable timestamps and actagram in timelapse output video.',
+)
+
+parser.add_argument('dirs', metavar='dirs', type=str, nargs='+',
+                    help='Directories to read')
+
+args = parser.parse_args()
+print (args)
+
+if args.outputfile:
+    outname = args.outputfile
+else:
+    # Come up with a reasonable output name.
+    outname = "timelapse"
+    if len(args.dirs) == 1: outname = args.dirs[0]
+    
+    if outname[1] == ":": outname = outname[2:]
+    outname = outname.replace("\\","_").replace("/","_")
+    if outname[0] == "_": outname = outname[1:]
+    if outname == "": outname = "timelapse"
+    outname = outname + ".avi"
+
+print ("out name:" ,outname)
+print (args.dirs)
 
 
-images = glob.glob("images/*.jpg")
-print ("Number of images:",len(images))
-images = sorted(images)
+images = []
+for pn in args.dirs:
+    if pn.find("*") == -1 and pn.find("?") == -1:
+        if pn[-1] != '/':
+            pn += '/*.jpg'
+        else:
+            pn += "*.jpg"
+
+    print("get",pn)
+    dir_images = glob.glob(pn)
+    dir_images = sorted(dir_images)
+    images = images + dir_images
+
 frames = 0
 
-timestamps = []   # in output frames
-frameseconds = [] # in output frames
+timestamps = []   # one entry per output frame
+frameseconds = [] # one entry per output frame
 
-seconds = [] # in input frames
+seconds = [] # one entry per image
 
 # Make image list to specify input frames for the timelapse part.
 with open("imglist.txt","w") as imglist:
     prevname = ""
+    imglist2 = []
 
     def WriteEntry(filename, imgframes, second):
         global vidtime, lastsubtotime, frames, timestamps
-        imglist.write("file '"+filename+"'\n")
-        imglist.write("duration "+str(imgframes/framerate)+"\n");
-        frames += imgframes
         for i in range (imgframes):
+            imglist.write("file '"+filename+"'\n")
             timestamps.append(os.path.basename(filename)[0:11])
             frameseconds.append(second)
+            frames += 1
 
+
+    nr = re.compile(r"""(\d\d\d\d)-(\d\d)(\d\d)(\d\d)""", re.ASCII)
+    prevsecond = -100
 
     for i in images:
         bi= os.path.basename(i)
 
-        try:
-            second = int(bi[5:7])*3600
-            second += int(bi[7:9])*60 + int(bi[9:11])
-            seconds.append(second)
-        except:
-            # Not a numbered file.  Skip
+        m = nr.match(bi)
+        if not m: 
+            print("Skipping file:",i)
             continue
 
-        if prevname != "":
-            gap = second-prevsecond
+        second = int(m.group(2))*3600+int(m.group(3))*60+int(m.group(4))
+            
+        seconds.append(second)
+        imglist2.append(i)
 
-            nf = 1
-            if gap > 5: nf = 2
+        gap = second-prevsecond
 
-            WriteEntry(prevname,nf, prevsecond)
+        nf = 1
+        if gap > 10: nf = 2
+
+        WriteEntry(i,nf, second)
 
         prevsecond = second
-        prevname = i
 
-    if prevname != "": WriteEntry(prevname, 3, prevsecond)
+    images = imglist2 # Replace list with one with unused files removed.
 
+print ("Number of images:",len(images))
+size = Image.open(images[0]).size
+size2 = Image.open(images[-1]).size
+if size != size2:
+    print("Size changes from ",size, "to", size2)
+imgwidth,imgheight = size
 
+print("Image size: ",size)
 print("Output frames = ",frames)
-#print (timestamps)
 
 firstsec = int(seconds[0] /3600)*3600
+hours = int((seconds[-1]+3600-1-firstsec)/3600)
+print (hours)
 
-print(seconds)
-print ("Timestamp array len",len(timestamps))
-
-
-
-# Create image of histogram bargraph to go here
-HIST_BINS = 240
-imgwidth = 2400
+# Parameters for histogram bagraph.
+HIST_BINS = 240*hours
+SEC_PER_BIN = 3600/240
 ActBins = [0] * HIST_BINS
-per_hist_bar = imgwidth/HIST_BINS
-BARS_HEIGHT=100
+hist_left = 300
+hist_width = imgwidth-hist_left
+per_hist_bar = hist_width/HIST_BINS
+actagram_top=5
+actagram_height=60
+actagram_bot = actagram_top+actagram_height
 
-max_bin = 6;# maximun used to scale histogram within canvas height
+max_bin = 6;# maximun used to scale histogram within canvas height, if < 6 use 6.
 for sec in seconds:
-    binfl = (sec-firstsec)*HIST_BINS/3600
-    bin = int(binfl)
-    ActBins[bin] += 1
-    if max_bin < ActBins[bin]: max_bin = ActBins[bin];
+    bin = int((sec-firstsec)/SEC_PER_BIN)
+    if bin < HIST_BINS:
+        n = ActBins[bin] + 1
+        ActBins[bin] = n
+    if max_bin < n: max_bin = n;
 
 def MakeActagram():
-    Actagram = Image.new("RGB", (imgwidth, 100), (30,30,30))
-
-    # create rectangle image
+    Actagram = Image.new("RGB", (imgwidth, actagram_top+actagram_height), (0,0,0))
     imgdraw = ImageDraw.Draw(Actagram)
-    shape = [(200, 2), (600,20)]
-    imgdraw.rectangle(shape, fill ="#ff2233", outline ="red")
 
-
-    ## draw 10 minute scale stripe background as first layer
-    for a in range (0,60,10):
-        histX = a*HIST_BINS/60*per_hist_bar
-        fc = "#000000"
-        if a % 20 == 0: fc = "#282828"
-        imgdraw.rectangle([(histX,0),(histX+per_hist_bar*HIST_BINS/6,100)], fill=fc)
-
+    # draw 10 minute scale stripe background as background layer.
+    for a in range (0,HIST_BINS,int(1200/SEC_PER_BIN)):
+        histX = a*per_hist_bar+hist_left
+        fc = "#303030"
+        imgdraw.rectangle([(histX,actagram_top),
+                           (histX+per_hist_bar*600/SEC_PER_BIN,actagram_bot)],
+                           fill=fc)
+    # Draw the bars.
     for a in range(HIST_BINS):
         b = ActBins[a];
         if b == 0: continue
 
-        rectHeight = b/max_bin*BARS_HEIGHT;
-        print(a)
-
-        imgdraw.rectangle([(a*per_hist_bar,100),((a+1)*per_hist_bar+1,100-rectHeight)], fill="#a0a0a0")
+        rectHeight = b/max_bin*actagram_height;
+        
+        histX = a*per_hist_bar+hist_left
+        imgdraw.rectangle([(histX,actagram_bot),(histX + per_hist_bar-2,actagram_bot-rectHeight)], fill="#a0a0a0")
 
     #Actagram.show()
     return Actagram
 
 Actagram = MakeActagram()
 
-p = Popen(['ffmpeg', '-y',
+pargs = ['ffmpeg', '-y', '-hide_banner',
      # Parameters for timelapse images
-     '-f', 'concat', '-safe', '0', '-i', 'imglist.txt',
+     '-f', 'concat', '-safe', '0', '-r', str(framerate), '-i', 'imglist.txt']
 
+if not args.no_timestamp:
+    pargs = pargs + [
      # Parameters for actagram
      '-f', 'image2pipe', '-vcodec', 'mjpeg', '-r', str(framerate), '-i', '-',
 
      # How to combine them
-     '-filter_complex', 'vstack',
+     '-filter_complex', 'vstack']
 
+pargs = pargs + [
      # Output prameters
-     '-vcodec', 'mpeg4', '-qscale', '3', '-r', str(framerate), 'video.avi'], stdin=PIPE)
+     '-vcodec', 'mpeg4', '-qscale', '10', '-r', str(framerate), outname]
 
-font = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\arial.ttf', 40)
-im = Image.new("RGB", (2400, 50), (30,30,30))
-for i in range(frames):
-    im = Actagram.copy()
-    draw = ImageDraw.Draw(im)
-    draw.text((5, 5), timestamps[i], font = font, align ="left")
+p = Popen(pargs, stdin=PIPE)
 
-    second = frameseconds[i]
-    imx = (second-firstsec)/3600*imgwidth
-    draw.rectangle([(imx-1,0),(imx+1,100)], fill="#ff00ff")
+textsize = 40
+font = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\arial.ttf', textsize)
 
-    im.save(p.stdin, 'JPEG', quality=90)
+if not args.no_timestamp:
+    for i in range(frames):
+        im = Actagram.copy()
+        draw = ImageDraw.Draw(im)
+        draw.text((0,(actagram_height-textsize)/2+actagram_top), timestamps[i], font = font, align ="left")
+
+        second = frameseconds[i]
+        imx = (second-firstsec)/SEC_PER_BIN*per_hist_bar+hist_left
+        draw.rectangle([(imx-1,actagram_top),(imx+1,actagram_bot)], fill="#ffff00")
+
+        im.save(p.stdin, 'JPEG', quality=90)
+        
 p.stdin.close()
 p.wait()
 
 #Still to do:
-# Get size from images and scale actagram accordingly
-# Make actagram not full width to leave room for timestamp
 # Test if images are not all same size
-# Handle doing more than one hour's worth, from multiple directories
-# --outputfile option
-# --pathname option
-# --no-timestamp option
-# --no-actagram option
